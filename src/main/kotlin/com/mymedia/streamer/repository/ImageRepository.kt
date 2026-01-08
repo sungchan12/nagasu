d3aaad7358fd99f67713dac0a9854245c5859690
sungchan12 <chanyouunng1@gmail.com>
2026-01-08 16:52:39 +0900

    1. 반환값이 File형식이 아닌 url방식으로 리턴하게 변경 2. Service쪽에서 metadata 읽어오거나 저장하는게 아닌 Repository에서 읽고 저장하는걸로 변경

--- src/main/kotlin/com/mymedia/streamer/repository/ImageRepository.kt
+++ src/main/kotlin/com/mymedia/streamer/repository/ImageRepository.kt
@@ -1,25 +1,39 @@
 package com.mymedia.streamer.repository
 
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
-import com.fasterxml.jackson.module.kotlin.readValue
 import com.mymedia.streamer.dto.metadata.ImageMetadata
 import com.mymedia.streamer.utils.isImageFile
+import com.fasterxml.jackson.module.kotlin.readValue
 import java.io.File
+
+private val objectMapper = jacksonObjectMapper()
 
 fun File.getCollectionDirs(): List<File> {
     return this.listFiles()?.filter { it.isDirectory } ?: emptyList()
 }
 
-fun File.getImageFiles(): List<File> {
-    return this.walkTopDown().filter { it.isImageFile()}.sortedBy { it.name }.toList()
+fun File.getImageFileNames(): List<String> {
+    return this.walkTopDown().filter { it.isImageFile() }.map { it.name }.sorted().toList()
 }
 
-fun File.getThumbnailImageFile(): File? {
+fun File.getThumbnailFileName(): String? {
     if (!this.isDirectory) return null
     val thumbnailFile = this.listFiles()
         ?.find { it.nameWithoutExtension.lowercase() == "thumbnail" && it.isImageFile() }
-    if (thumbnailFile != null) return thumbnailFile
+    if (thumbnailFile != null) return thumbnailFile.name
     return this.listFiles()
         ?.filter { it.isImageFile() }
         ?.minByOrNull { it.name }
+        ?.name
+}
+
+fun File.getMetaData(): ImageMetadata? {
+    val metadataFile = File(this, "metadata.json")
+    if (!metadataFile.exists()) return null
+    return objectMapper.readValue<ImageMetadata>(metadataFile)
+}
+
+fun File.saveMetaData(metadata: ImageMetadata) {
+    val metadataFile = File(this, "metadata.json")
+    objectMapper.writeValue(metadataFile, metadata)
 }
