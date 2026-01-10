35dad71e5dba386b6e4db402288a08be27d019d4
sungchan12 <chanyouunng1@gmail.com>
2026-01-10 16:59:16 +0900

    DTO에 tags 요소 추가

--- src/main/kotlin/com/mymedia/streamer/dto/VideoCollectionResponse.kt
+++ src/main/kotlin/com/mymedia/streamer/dto/VideoCollectionResponse.kt
@@ -4,8 +4,9 @@ package com.mymedia.streamer.dto
  * 비디오 컬렉션(폴더) 정보를 반환하는 응답 DTO
  */
 data class VideoCollectionResponse(
-    val id: String,           // 컬렉션 ID (폴더 경로)
-    val name: String,         // 컬렉션 이름 (폴더명)
-    val thumbnailUrl: String, // 썸네일 URL
-    val fileCount: Int        // 비디오 파일 개수
+    val id: String,
+    val title: String,
+    val artist: String,
+    val tags: List<String>,
+    val thumbnailUrl: String
 )
