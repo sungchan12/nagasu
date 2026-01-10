ecac26811994f4711c92d833c07eaf75189ddbed
sungchan12 <chanyouunng1@gmail.com>
2026-01-10 16:57:17 +0900

    사용하지 않는 import문 삭제

--- src/main/kotlin/com/mymedia/streamer/controller/ImageController.kt
+++ src/main/kotlin/com/mymedia/streamer/controller/ImageController.kt
@@ -12,13 +12,8 @@ import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.ModelAttribute
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestParam
-import org.springframework.web.bind.annotation.RequestPart
 import org.springframework.web.bind.annotation.RestController
-import org.springframework.web.multipart.MultipartFile
-import java.awt.Image
 
 /**
  * 이미지 컬렉션 API 컨트롤러
