74a441d49a6d1dd6b04e200555f50f2079ba8d43
sungchan12 <chanyouunng1@gmail.com>
2026-01-10 17:00:41 +0900

    util과 repository 분리

--- src/main/kotlin/com/mymedia/streamer/repository/ImageRepository.kt
+++ src/main/kotlin/com/mymedia/streamer/repository/ImageRepository.kt
@@ -1,12 +1,7 @@
 package com.mymedia.streamer.repository
 
-import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
-import com.mymedia.streamer.dto.metadata.ImageMetadata
 import com.mymedia.streamer.utils.isImageFile
-import com.fasterxml.jackson.module.kotlin.readValue
 import java.io.File
-
-private val objectMapper = jacksonObjectMapper()
 
 fun File.getCollectionDirs(): List<File> {
     return this.listFiles()?.filter { it.isDirectory } ?: emptyList()
@@ -25,15 +20,4 @@ fun File.getThumbnailFileName(): String? {
         ?.filter { it.isImageFile() }
         ?.minByOrNull { it.name }
         ?.name
-}
-
-fun File.getMetaData(): ImageMetadata? {
-    val metadataFile = File(this, "metadata.json")
-    if (!metadataFile.exists()) return null
-    return objectMapper.readValue<ImageMetadata>(metadataFile)
-}
-
-fun File.saveMetaData(metadata: ImageMetadata) {
-    val metadataFile = File(this, "metadata.json")
-    objectMapper.writeValue(metadataFile, metadata)
 }
