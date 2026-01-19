654bc09f3a69760c7e7468b3a2a9337f5856a5a8
sungchan12 <chanyouunng1@gmail.com>
2026-01-19 23:56:02 +0900

    video repository 추가

--- src/main/kotlin/com/mymedia/streamer/repository/VideoRepository.kt
+++ src/main/kotlin/com/mymedia/streamer/repository/VideoRepository.kt
@@ -1,8 +1,13 @@
 package com.mymedia.streamer.repository
 
-import com.mymedia.streamer.dto.VideoCollectionResponse
+import com.mymedia.streamer.utils.isVideoFile
 import java.io.File
 
-fun File.getVideoCollection(): List<File> {
-    return this.listFiles()?.filter { it.isDirectory } ?: emptyList()
+fun File.getVideoCollection(): List<String> {
+    return this.listFiles()?.filter { it.isDirectory }?.map { it.name } ?: emptyList()
+}
+
+fun File.getVideoFileName(): String? {
+    if (!this.isDirectory) return null
+    return this.listFiles()?.firstOrNull { it.isVideoFile() }?.name
 }
