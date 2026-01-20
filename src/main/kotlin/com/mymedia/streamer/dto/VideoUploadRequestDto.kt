70130c244ebcffd3c7f484368549d9724b6dcb0b
sungchan12 <chanyouunng1@gmail.com>
2026-01-21 00:23:03 +0900

    자막 dto 추가

--- src/main/kotlin/com/mymedia/streamer/dto/VideoUploadRequestDto.kt
+++ src/main/kotlin/com/mymedia/streamer/dto/VideoUploadRequestDto.kt
@@ -8,5 +8,6 @@ data class VideoUploadRequestDto(
     val tags: List<String> = emptyList(),
     val description: String?,
     val video: MultipartFile,
-    val thumbnail: MultipartFile? = null
+    val thumbnail: MultipartFile? = null,
+    val subtitle: MultipartFile? = null
 )
