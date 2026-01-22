6d3a007f74e861ce76d3127d34ff24badceb6266
sungchan12 <chanyouunng1@gmail.com>
2026-01-22 19:40:46 +0900

    업로드 Max 에러 처리

--- src/main/kotlin/com/mymedia/nagasu/config/GlobalExceptionHandler.kt
+++ src/main/kotlin/com/mymedia/nagasu/config/GlobalExceptionHandler.kt
@@ -5,6 +5,7 @@ import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.RestControllerAdvice
+import org.springframework.web.multipart.MaxUploadSizeExceededException
 
 @RestControllerAdvice
 class GlobalExceptionHandler {
@@ -20,11 +21,30 @@ class GlobalExceptionHandler {
             ))
     }
 
+    @ExceptionHandler(NoSuchElementException::class)
+    fun handleNoSuchElement(e: NoSuchElementException): ResponseEntity<Map<String, String>> {
+        logger.warn("Element not found: ${e.message}")
+        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+            .body(mapOf(
+                "error" to "Not Found",
+                "message" to (e.message ?: "Request resource not found")
+            ))
+    }
+
     @ExceptionHandler(IllegalStateException::class)
     fun handleIllegalStateException(e: IllegalStateException): ResponseEntity<Map<String, String>> {
         logger.warn("StateException: ${e.message}")
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(mapOf(
             "error" to "Internal Server Error",
+            "message" to (e.message ?: "An internal error occurred")
+        ))
+    }
+
+    @ExceptionHandler(MaxUploadSizeExceededException::class)
+    fun handleMaxUploadSizeExceededException(e: MaxUploadSizeExceededException): ResponseEntity<Map<String, String>> {
+        logger.warn("MaxUploadSizeExceededException: ${e.message}")
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(mapOf(
+            "error" to "Max Upload Size Exceeded",
             "message" to (e.message ?: "An internal error occurred")
         ))
     }
