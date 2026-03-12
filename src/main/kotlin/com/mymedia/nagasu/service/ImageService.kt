3eed6be6248794853641ed7a696d3458f4d07546
sungchan12 <chanyouunng1@gmail.com>
2026-03-12 23:43:53 +0900

    코드리뷰쪽 수정

--- src/main/kotlin/com/mymedia/nagasu/service/ImageService.kt
+++ src/main/kotlin/com/mymedia/nagasu/service/ImageService.kt
@@ -22,7 +22,6 @@ import org.springframework.stereotype.Service
 import org.springframework.web.multipart.MultipartFile
 import java.io.File
 import java.nio.file.Path
-import java.util.Collections.list
 
 /**
  * Image collection management service
