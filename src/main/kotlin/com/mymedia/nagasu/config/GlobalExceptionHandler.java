e1e90468a25a6c8408df409ebc2b966ebb22e028
sungchan12 <chanyouunng1@gmail.com>
2026-03-13 22:55:39 +0900

    재미로 java로 코드 변경해봤습니다

---
+++ src/main/kotlin/com/mymedia/nagasu/config/GlobalExceptionHandler.java
@@ -0,0 +1,32 @@
+package com.mymedia.nagasu.config;
+
+import com.mymedia.nagasu.dto.ApiResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.nio.file.NoSuchFileException;
+import java.util.NoSuchElementException;
+
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    private static final Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+
+    @ExceptionHandler(NoSuchFileException.class)
+    public ResponseEntity<ApiResponse<?>> handleNotFound(NoSuchFileException e) {
+        logger.warn("File not found: {}", e.getMessage());
+        var errorBody = new ApiResponse.Failure(e.getMessage() != null ? e.getMessage() : "File not Found");
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorBody);
+    }
+
+    @ExceptionHandler(NoSuchElementException.class)
+    public ResponseEntity<ApiResponse<?>> handleIllegalStateException(IllegalStateException e) {
+        logger.warn("StateException: {}", e.getMessage());
+        var errorBody = new ApiResponse.Failure(e.getMessage() != null ? e.getMessage() :  "An internal error occurred");
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorBody);
+    }
+}
