a1e4d18f378603a5a283cb9a432cd41d18120c52
sungchan12 <chanyouunng1@gmail.com>
2026-03-14 20:18:38 +0900

    의존성 변경

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,10 +27,7 @@ repositories {
 
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-validation")
-	implementation("org.springframework.boot:spring-boot-starter-web") {
-		exclude(group = "org.springframework.boot", module = "spring-boot-starter-tomcat")
-	}
-	implementation("org.springframework.boot:spring-boot-starter-undertow")
+	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	compileOnly("org.projectlombok:lombok")
