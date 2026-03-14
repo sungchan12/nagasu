00785412d3b051ea3ca32dc040ef466612ad244d
sungchan12 <chanyouunng1@gmail.com>
2026-03-14 20:18:29 +0900

    RuntimeException Error Handling 추가

--- src/main/java/com/mymedia/nagasu/config/GlobalExceptionHandler.java
+++ src/main/java/com/mymedia/nagasu/config/GlobalExceptionHandler.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.multipart.MaxUploadSizeExceededException;
 
 import java.nio.file.NoSuchFileException;
 import java.util.NoSuchElementException;
@@ -19,21 +20,42 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(NoSuchFileException.class)
     public ResponseEntity<ApiResponse<?>> handleNotFound(NoSuchFileException e) {
         logger.warn("File not found: {}", e.getMessage());
-        var errorBody = new ApiResponse.Failure(e.getMessage() != null ? e.getMessage() : "File not Found");
+        var errorBody = new ApiResponse.Failure<>(e.getMessage() != null ? e.getMessage() : "File not Found");
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorBody);
     }
 
     @ExceptionHandler(NoSuchElementException.class)
-    public ResponseEntity<ApiResponse<?>> handleIllegalStateException(IllegalStateException e) {
+    public ResponseEntity<ApiResponse<?>> handleNoSuchElement(NoSuchElementException e) {
         logger.warn("Element not Found: {}", e.getMessage());
-        var errorBody = new ApiResponse.Failure(e.getMessage() != null ? e.getMessage() :  "Element Not Found");
+        var errorBody = new ApiResponse.Failure<>(e.getMessage() != null ? e.getMessage() : "Element Not Found");
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorBody);
     }
 
     @ExceptionHandler(IllegalStateException.class)
     public ResponseEntity<ApiResponse<?>> handleIllegalStateException(IllegalStateException e) {
         logger.warn("StateException: {}", e.getMessage());
-        var errorBody = new ApiResponse.Failure(e.getMessage() != null ? e.getMessage() : "An internal error occurred");
+        var errorBody = new ApiResponse.Failure<>(e.getMessage() != null ? e.getMessage() : "An internal error occurred");
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorBody);
+    }
+
+    @ExceptionHandler(MaxUploadSizeExceededException.class)
+    public ResponseEntity<ApiResponse<?>> handleMaxUploadSizeExceededException(MaxUploadSizeExceededException e) {
+        logger.warn("MaxUploadSizeExceededException: {}", e.getMessage());
+        var errorBody = new ApiResponse.Failure<>(e.getMessage() != null ? e.getMessage() : "Max upload size exceeded");
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorBody);
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<ApiResponse<?>> handleIllegalArgumentException(IllegalArgumentException e) {
+        logger.warn("IllegalArgumentException: {}", e.getMessage());
+        var errorBody = new ApiResponse.Failure<>(e.getMessage() != null ? e.getMessage() : "IllegalArgumentException");
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorBody);
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<ApiResponse<?>> handleRuntimeException(RuntimeException e) {
+        logger.warn("RuntimeException: {}", e.getMessage());
+        var errorBody = new ApiResponse.Failure<>(e.getMessage() != null ? e.getMessage() : "A runtime error occurred");
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorBody);
     }
 }
