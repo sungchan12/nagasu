8105a0959eaff244fa321d5a676f1a4e48a94470
sungchan12 <chanyouunng1@gmail.com>
2026-03-21 23:56:58 +0900

    cookie 인증 허용

--- src/main/java/com/mymedia/nagasu/config/WebConfig.java
+++ src/main/java/com/mymedia/nagasu/config/WebConfig.java
@@ -21,6 +21,7 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowedOrigins("http://localhost:5173")
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
+                .allowCredentials(true)
                 .exposedHeaders("Content-Type", "Content-Length", "Accept-Ranges", "Content-Range");
     }
 
