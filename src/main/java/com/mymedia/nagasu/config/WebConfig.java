c7d51451c5392d0612d16db990942b88c10d42c6
sungchan12 <chanyouunng1@gmail.com>
2026-03-26 23:57:01 +0900

    도매인 수정

--- src/main/java/com/mymedia/nagasu/config/WebConfig.java
+++ src/main/java/com/mymedia/nagasu/config/WebConfig.java
@@ -18,7 +18,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173")
+                .allowedOrigins("https://yourdomain.dev")
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true)
