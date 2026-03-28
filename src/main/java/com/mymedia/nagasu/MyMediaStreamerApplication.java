5ff6d5ad84bca8a6970b9e54861b211c3185c581
sungchan12 <chanyouunng1@gmail.com>
2026-03-28 22:23:21 +0900

    Util로 분리한 수정, 삭제 video, image에 적용

--- src/main/java/com/mymedia/nagasu/MyMediaStreamerApplication.java
+++ src/main/java/com/mymedia/nagasu/MyMediaStreamerApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MyMediaStreamerApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(MyMediaStreamerApplication.class, args);
     }
